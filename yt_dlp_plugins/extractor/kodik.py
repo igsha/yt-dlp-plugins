23a6698b9d3757d54f6c7070a6e0ef9dd9fb97db
Igor Sharonov <igsha@users.noreply.github.com>
2024-06-12 22:17:31 +0300

    kodik: Return correct playlist

--- yt_dlp_plugins/extractor/kodik.py
+++ yt_dlp_plugins/extractor/kodik.py
@@ -15,25 +15,29 @@ class KodikIE(InfoExtractor):
         decoded = base64.b64decode(data.translate(__class__._decode_table))
         return re.sub(r'^//', 'https://', decoded.decode())
 
-    def __getter(self, domain, video_type, item):
+    def __getter(self, domain, video_type, title, item):
         video_id = item.get('data-id')
         video_hash = item.get('data-hash')
         video_value = item.get('value')
+        video_title = item.get('data-title')
 
         params = {'type': video_type, 'id': video_id, 'hash': video_hash}
         formdata = parse.urlencode(params).encode()
         webpage = self._download_webpage(f"{domain}/ftor", "Kodik GetVideoInfo", data=formdata)
 
         jsn = json.loads(webpage)
-        result = {'id': video_id, 'title': f"#{video_value} ({video_id})", 'episode_number': int(video_value)}
+        kwargs = dict(ie=None, video_id=video_id, video_title=f"{title} - {video_title}",
+                      episode_number=int(video_value))
         if 'link' in jsn:
-            result.update({'url': jsn['link'], 'protocol': 'm3u8'})
+            url = jsn['link']
         else:
             video_urls = jsn['links']
-            formats = [{'url': self.__decode(v[0]['src']), 'quality': int(k)} for (k,v) in video_urls.items()]
-            result.update({'formats': formats})
+            qualities = sorted(video_urls.keys(), key=int, reverse=True)
+            kwargs['formats'] = [{'url': self.__decode(video_urls[k][0]['src']), 'quality': int(k)}
+                                 for k in qualities]
+            url = kwargs['formats'][0]['url']
 
-        return result
+        return self.url_result(url, **kwargs)
 
     def _real_extract(self, url):
         video_id = self._match_id(url)
@@ -48,4 +52,5 @@ class KodikIE(InfoExtractor):
         items = t.xpath("//div[@class='serial-series-box']/*/option")
 
         self.report_extraction(title)
-        return {'_type': 'playlist', 'id': video_id, 'title': title, 'entries': map(lambda x: self.__getter(domain, video_type, x), items)}
+        return self.playlist_result(map(lambda x: self.__getter(domain, video_type, title, x), items),
+                                    video_id, title, playlist_count=len(items))
