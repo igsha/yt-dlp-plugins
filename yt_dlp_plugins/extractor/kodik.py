6c7bd256e8c548672900d6b146206af4976d9485
Igor Sharonov <igsha@users.noreply.github.com>
2024-06-15 20:22:40 +0300

    kodik: Use firect link to ftor

--- yt_dlp_plugins/extractor/kodik.py
+++ yt_dlp_plugins/extractor/kodik.py
@@ -5,7 +5,7 @@ from lxml import etree
 
 
 class KodikIE(InfoExtractor):
-    _VALID_URL = r'(?P<domain>(?:https?://)?(?:www\.)?(kodik|aniqit|anivod)\.[^/]+)/(?P<type>(serial?|season|video|uv))/(?P<id>[-\w/]+)'
+    _VALID_URL = r'(?P<domain>(?:https?://)?(?:www\.)?(kodik|aniqit|anivod)\.[^/]+)/ftor\?(?P<item>.+)'
     _decode_table = {
             **{c: c + 13 if c < ord('N') else c - 13 for c in range(ord('A'), ord('Z') + 1)},
             **{c: c + 13 if c < ord('n') else c - 13 for c in range(ord('a'), ord('z') + 1)}}
@@ -15,36 +15,30 @@ class KodikIE(InfoExtractor):
         decoded = base64.b64decode(data.translate(__class__._decode_table))
         return re.sub(r'^//', 'https://', decoded.decode())
 
-    def __getter(self, domain, video_type, title, item):
-        video_id = item.get('data-id')
-        video_hash = item.get('data-hash')
-        video_value = item.get('value')
-        video_title = item.get('data-title')
-
-        params = {'type': video_type, 'id': video_id, 'hash': video_hash}
-        formdata = parse.urlencode(params).encode()
-        webpage = self._download_webpage(f"{domain}/ftor", "Kodik GetVideoInfo", data=formdata)
-
+    def _real_extract(self, url):
+        item = dict(parse.parse_qsl(re.search(self._VALID_URL, url).group('item')))
+        webpage = self._download_webpage(url, item['id'])
+        title = item['title'] if 'title' in item else 'title'
+        episode = int(item['episode']) if 'episode' in item else 0
         jsn = json.loads(webpage)
-        kwargs = dict(ie=None, video_id=video_id, video_title=f"{title} - {video_title}",
-                      episode_number=int(video_value))
+        result = dict(id=item['id'], title=title, episode_number=episode)
         if 'link' in jsn:
-            url = jsn['link']
+            result['url'] = jsn['link']
         else:
             video_urls = jsn['links']
             qualities = sorted(video_urls.keys(), key=int, reverse=True)
-            kwargs['formats'] = [{'url': self.__decode(video_urls[k][0]['src']), 'quality': int(k)}
+            result['formats'] = [{'url': self.__decode(video_urls[k][0]['src']), 'quality': int(k)}
                                  for k in qualities]
-            url = kwargs['formats'][0]['url']
 
-        return self.url_result(url, **kwargs)
+        return result
+
+
+class KodikListIE(InfoExtractor):
+    _VALID_URL = r'(?P<domain>(?:https?://)?(?:www\.)?(kodik|aniqit|anivod)\.[^/]+)/(?P<type>(serial?|season))/(?P<id>[-\w/]+)'
 
     def _real_extract(self, url):
         video_id = self._match_id(url)
         domain = re.search(self._VALID_URL, url).group('domain')
-        video_type = re.search(self._VALID_URL, url).group('type')
-        if video_type == 'serial' or video_type == 'season':
-            video_type = 'seria'
 
         webpage = self._download_webpage(url, video_id, headers={'referer': domain})
         t = etree.HTML(webpage)
@@ -52,5 +46,12 @@ class KodikIE(InfoExtractor):
         items = t.xpath("//div[@class='serial-series-box']/*/option")
 
         self.report_extraction(title)
-        return self.playlist_result(map(lambda x: self.__getter(domain, video_type, title, x), items),
-                                    video_id, title, playlist_count=len(items))
+        urls = []
+        for item in items:
+            # episode and title is a fake arguments
+            params = {k:item.get(v) for k, v in [('id', 'data-id'), ('hash', 'data-hash'), ('episode', 'value')]}
+            params['title'] = title + " - " + item.get('data-title')
+            params['type'] = 'seria'
+            urls.append(self.url_result(f"{domain}/ftor?{parse.urlencode(params)}", ie=KodikIE.ie_key(), title=item.get('data-title')))
+
+        return self.playlist_result(urls, video_id, title, playlist_count=len(urls))
